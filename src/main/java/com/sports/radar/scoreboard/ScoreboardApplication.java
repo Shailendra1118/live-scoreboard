69991921fa7d78e1ba70269ec61191527ce123ce
Shailendra Singh Yadav <shailendra1118@gmail.com>
2022-09-28 19:16:23 +0530

    spring initializr

---
+++ src/main/java/com/sports/radar/scoreboard/ScoreboardApplication.java
@@ -0,0 +1,14 @@
+package com.sports.radar.scoreboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoreboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoreboardApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/com/sports/radar/scoreboard/ScoreboardApplicationTests.java
@@ -0,0 +1,18 @@
+package com.sports.radar.scoreboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoreboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	public void testCreateGame() {
+		System.out.println("Craeted!");
+	}
+
+}
